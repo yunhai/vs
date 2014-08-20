1279c8c87b557a879d2fe655fafe975b64adbda7
yunhai <yunhaihuang@gmail.com>
2014-08-20 23:16:36 +0700

    clean up

--- cache/skins/user/New folder/skin_configs.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!class_exists('skin_objectpublic'))
-require_once ('./cache/skins/user/finance/skin_objectpublic.php');
-class skin_configs extends skin_objectpublic {
-
-
-
-}
-?>
--- cache/skins/user/New folder/skin_productlabels.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!class_exists('skin_objectpublic'))
-require_once ('./cache/skins/user/finance/skin_objectpublic.php');
-class skin_productlabels extends skin_objectpublic {
-
-
-
-}
-?>
