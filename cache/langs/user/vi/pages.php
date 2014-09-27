2e857e64863cc56d3bbe2a7fce9ecb9c013402dc
yunhai <yunhaihuang@gmail.com>
2014-09-27 17:02:35 +0700

    deleted

--- cache/langs/user/vi/pages.php
+++
@@ -1,6 +0,0 @@
-<?php $langs=array (
-  'module_get_class_success' => 'Get action successfully!',
-  'main_title' => 'Quản trị website',
-  'pageTitle' => 'Trang chủ',
-  'develop_get_obj_success' => 'Execute successful',
-)?>
